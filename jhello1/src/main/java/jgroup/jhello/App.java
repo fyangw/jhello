d0a61aeab9a9b40bcf65c3f2a57a178bef37f4d7
fyangw <fyangw@github.com>
2019-09-21 17:52:56 +0800

    init maven quickstart helloworld

---
+++ jhello1/src/main/java/jgroup/jhello/App.java
@@ -0,0 +1,13 @@
+package jgroup.jhello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
